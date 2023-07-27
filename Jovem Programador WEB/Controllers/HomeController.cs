4d27f24ef71c02772d18b8c741e10e3c56fac1c6
otavio.silva2 <otavio.silva2@BRE203D26>
2023-07-26 21:15:06 -0300

    Adicionar arquivos de projeto.

---
+++ Jovem Programador WEB/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Jovem_Programador_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Jovem_Programador_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Aluno()
+        {
+            return View();
+        }
+
+        public IActionResult Professor()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
