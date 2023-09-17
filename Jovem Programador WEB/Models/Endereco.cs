c5f279c2a89098cbe3a3417a65bccc8ca50bd3b5
TavinSs <otaviomdie@gmail.com>
2023-09-17 17:10:36 -0300

    Não entendi nada

--- Jovem Programador WEB/Models/Endereco.cs
+++ Jovem Programador WEB/Models/Endereco.cs
@@ -2,7 +2,7 @@
 {
     public class Endereco
     {
-        public string cep { get; set; }
+        public string cep { get; }
         public string logradouro { get; set; }
         public string complemento { get; set; }
         public string bairro { get; set; }
